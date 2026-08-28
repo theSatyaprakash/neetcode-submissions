2760e2c6938a785a13397540fe338bf8995aa47d
Satya Prakash Swain <satyaprakash1526@gmail.com>
2026-08-28 22:54:05 +0530

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.py
@@ -0,0 +1,29 @@
+class TimeMap:
+
+    def __init__(self):
+        self.map = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.map:
+            self.map[key] = []
+        self.map[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.map:
+            return ""
+
+        values = self.map[key]
+        left = 0
+        right = len(values) - 1
+        result = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if values[mid][0] <= timestamp:
+                result = values[mid][1]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return result
